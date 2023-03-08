48f029c6205d5d7909405dc08ca75e3dfd232985
akmalranyan <akmalranyan@gmail.com>
2023-03-08 14:20:55 +0700

    custom return api detail post resource

--- app/Http/Resources/PostDetailResource.php
+++ app/Http/Resources/PostDetailResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
+class PostDetailResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,14 +14,12 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-       //return parent::toArray($request);
-       return [
-        'id' => $this->id,
-        'title' => $this->title,
-        'news_content' => $this->news_content,
-        'created_at' => date_format($this -> created_at, "Y/m/d H:i:s"),
-       ];
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'news_content' => $this->news_content,
+            'author' => $this->author,
+            'created_at' => date_format($this -> created_at, "Y/m/d H:i:s"),
+           ];
     }
-
-    
 }
