b1e38350a60ea99008f0372c566de8a4f0bcd7f3
akmalranyan <akmalranyan@gmail.com>
2023-03-16 14:14:23 +0700

    create comment on some post

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function store(Request $request){
         $request -> validate([
-            'post_id' => 'required|exist:post,id',
-            'comment_content' => 'required'
+            'post_id' => 'required|exists:posts,id',
+            'commnets_content' => 'required'
         ]);
+        $request['user_id'] = auth()->user()->id;
+        $comment = Comment::create($request->all());
+
+        return response()->json($comment);
     }
 }
